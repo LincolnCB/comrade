b2828473c937824ae924d65b41005fae2de59486
LincolnCB <lcravenbrightman@gmail.com>
2024-01-11 13:53:11 -0800

    Added dummy pipeline and test directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+pub mod layout;
+pub mod matching;
+
 fn main() {
     println!("Hello, world!");
+
+    layout::do_layout();
+    matching::do_matching();
 }
---
+++ src/matching.rs
@@ -0,0 +1,3 @@
+pub fn do_matching() {
+    println!("Dummy matching");
+}
